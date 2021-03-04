5e52e165afad4c2fd51ed7b1df76d174e604a2f1
rod <rod@nayfield.com>
2021-03-04 18:15:21 -0500

    Add pagination, move uid to env

--- followers.py
+++ followers.py
@@ -5,19 +5,23 @@ import json
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 
+from dotenv import load_dotenv
+# The file '.env' contains two variables we care about:
+# BEARER_TOKEN=AAjkldjfdlksjfdskljklfds
+# TWITTER_UID=12345
+
 
 def auth():
     return os.environ.get("BEARER_TOKEN")
 
 
 def create_url():
-    # Replace with user ID below
-    user_id = 2244994945
+    user_id = os.environ.get("TWITTER_UID")
     return "https://api.twitter.com/2/users/{}/followers".format(user_id)
 
 
 def get_params():
-    return {"user.fields": "created_at"}
+    return {"user.fields": "public_metrics,created_at"}
 
 
 def create_headers(bearer_token):
@@ -36,13 +40,28 @@ def connect_to_endpoint(url, headers, params):
         )
     return response.json()
 
+def connect_paginated(url, headers, params):
+    retval = []
+    more = 'firstrun'
+    while more:
+        if more != 'firstrun':
+            params['pagination_token'] = more
+        got = connect_to_endpoint(url, headers, params)
+        retval.extend(got['data'])
+        if 'next_token' in got['meta']:
+            print('more', got['meta']['next_token'])
+            more = got['meta']['next_token']
+        else:
+            more = False
+    return retval
 
 def main():
+    load_dotenv()
     bearer_token = auth()
     url = create_url()
     headers = create_headers(bearer_token)
     params = get_params()
-    json_response = connect_to_endpoint(url, headers, params)
+    json_response = connect_paginated(url, headers, params)
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
